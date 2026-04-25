6051ccb59465c95426d32f09f9f78326023a541f
csouhardya <kittu.chakrovartee@gmail.com>
2026-04-25 21:15:26 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace EventsAndDelegates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Video video = new() { Title = "Video1"};
+            var videoEncoder = new VideoEncoder(); // publisher
+            var mailService = new MailService();
+
+            videoEncoder.VideoEncodedEvent += mailService.OnVideoEncoded;
+
+            videoEncoder.Encode(video);
+        }
+    }
+
+    public class MailService
+    {
+        public void OnVideoEncoded(object source,  EventArgs args)
+        {
+            Console.WriteLine("MailService: sending one mail");
+        }
+    }
+}
---
+++ VideoEncoder.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventsAndDelegates
+{
+    public class VideoEncoder
+    {
+        // 1. define a delegate
+        // 2. define an event based on that delegate
+        // 3. raise an event
+
+
+        public delegate void VideoEncodedEventHandler(object source, EventArgs args);
+
+        public event VideoEncodedEventHandler VideoEncodedEvent;
+
+        public void Encode(Video video)
+        {
+            this.OnVideoEncoded();
+        }
+
+        protected virtual void OnVideoEncoded()
+        {
+            if (VideoEncodedEvent != null)
+                VideoEncodedEvent(this, EventArgs.Empty);
+        }
+    }
+}
